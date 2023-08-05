5babbf8d0ddf3650ae4da1c8d7625d99e22c1942
agamswaroop619 <agam.swaroop619@gmail.com>
2023-08-05 15:46:44 +0530

    5th Assignment

---
+++ src/_2104373_5.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class _2104373_5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        scanner.close();
+
+        if (isPalindrome(input)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
