11912d90beea546f72a76b7eff18b469bc97436c
agamswaroop619 <agam.swaroop619@gmail.com>
2023-08-05 15:55:17 +0530

    6th Assignment

---
+++ src/_2104373_6.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class _2104373_6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the temperature (e.g., C 37 or F 98.6): ");
+        String input = scanner.nextLine();
+        scanner.close();
+
+        char scale = input.charAt(0);
+        double temperature = Double.parseDouble(input.substring(2));
+
+        if (scale == 'C' || scale == 'c') {
+            double fahrenheit = celsiusToFahrenheit(temperature);
+            System.out.println("Fahrenheit: " + fahrenheit);
+        } else if (scale == 'F' || scale == 'f') {
+            double celsius = fahrenheitToCelsius(temperature);
+            System.out.println("Celsius: " + celsius);
+        } else {
+            System.out.println("Invalid input. Please specify the temperature scale (C or F).");
+        }
+    }
+
+    private static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    private static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+}
